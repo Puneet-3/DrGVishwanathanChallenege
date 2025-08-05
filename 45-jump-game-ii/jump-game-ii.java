fa4fdd513ed6dd48edc4da49193bb09540b73668
Puneet <157231636+Puneet-3@users.noreply.github.com>
2025-08-05 23:48:23 +0530

    Time: 2 ms (29.58%) | Memory: 45.4 MB (32.17%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int currentFarthest = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            currentFarthest = Math.max(currentFarthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = currentFarthest;
+            }
+        }
+        return jumps;
+    }
+}
